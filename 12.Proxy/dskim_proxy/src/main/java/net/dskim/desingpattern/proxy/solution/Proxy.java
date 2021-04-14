0804211551360fab3ebe11e9842b8d1db6f63a2e
dskim <bluedskim@gmail.com>
2021-04-14 12:46:23 +0900

    지연된 초기화 Lazy initialization 구현

--- 12.Proxy/dskim_proxy/src/main/java/net/dskim/desingpattern/proxy/solution/Proxy.java
+++ 12.Proxy/dskim_proxy/src/main/java/net/dskim/desingpattern/proxy/solution/Proxy.java
@@ -1,19 +1,23 @@
 package net.dskim.desingpattern.proxy.solution;
 
+import lombok.extern.slf4j.Slf4j;
 import net.dskim.desingpattern.proxy.HeavyResource;
 import net.dskim.desingpattern.proxy.Resource;
 
+@Slf4j
 public class Proxy implements Resource {
 	private HeavyResource heavyResource;
 
-	public Proxy() throws InterruptedException {
-		if(heavyResource == null) {
-			this.heavyResource = new HeavyResource();
-		}
+	public Proxy() {
+		super();
+		log.info("Proxy 생성 완료");
 	}
 
 	@Override
 	public String process() {
+		if (heavyResource == null) {
+			this.heavyResource = new HeavyResource();
+		}
 		return heavyResource.process();
 	}
 }
