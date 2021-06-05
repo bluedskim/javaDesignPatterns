bee5e4e0dd3364a6a4b2d9b4aaac775cbdb1dd4f
dskim <bluedskim@gmail.com>
2021-06-05 11:28:01 +0900

    assertSame 을 제일 마지막으로 이동

--- 01.Singleton/dskim_singleton/src/test/java/net/dskim/desingpattern/singleton/SingletonTest.java
+++ 01.Singleton/dskim_singleton/src/test/java/net/dskim/desingpattern/singleton/SingletonTest.java
@@ -13,17 +13,16 @@ public class SingletonTest {
 		Coin coin1 = Coin.getInstance();
 		Coin coin2 = Coin.getInstance();
 
-		log.info("coin1={}, coin2={}", coin1, coin2);
-		assertEquals(coin1, coin2);	//assertTrue(coin1.equals(coin2));
-		assertSame(coin1, coin2);
-
-		coin1.addMoreCoin();
-		
+		coin1.addMoreCoin();		
 		log.info("coin1.getCoinCount()={}, coin2.getCoinCount()={}", coin1.getCoinCount(), coin2.getCoinCount());
 		assertEquals(10, coin2.getCoinCount());
 
 		coin2.deductCoin();
 		log.info("coin1.getCoinCount()={}, coin2.getCoinCount()={}", coin1.getCoinCount(), coin2.getCoinCount());
 		assertEquals(9, coin1.getCoinCount());
+
+		log.info("coin1={}, coin2={}", coin1, coin2);
+		assertEquals(coin1, coin2);	// 이것과 동일함 assertTrue(coin1.equals(coin2));
+		assertSame(coin1, coin2);		
 	}
 }
